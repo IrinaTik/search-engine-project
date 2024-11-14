de6a53ed5f98ccd9cf3368371e8b95c85711a69b
IrinaTik <i.tikhomirova6@gmail.com>
2024-11-14 16:45:55 +0300

    feat(jpa): добавлен слой для работы с таблицами site и page в БД
    Добавлены классы для сущностей БД site и page, а также вспомогательный enum для статуса индексации сайта.
    
    Добавлены JPA репозитории для сущностей site и page.
    
    Этап: 2

---
+++ src/main/java/searchengine/model/PageEntity.java
@@ -0,0 +1,50 @@
+package searchengine.model;
+
+import lombok.*;
+
+import jakarta.persistence.*;
+import java.util.Objects;
+
+// Lombok
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+// Database
+@Entity
+@Table(name = "page", indexes = @Index(name = "path_idx", columnList = "path"))
+public class PageEntity {
+
+    @Id
+    @Column(nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.MERGE)
+    @JoinColumn(name = "site_id", nullable = false)
+    private SiteEntity site;
+
+    @Column(name = "path", nullable = false, columnDefinition = "VARCHAR(255) CHARACTER SET utf8mb4")
+    private String relativePath;
+
+    @Column(nullable = false)
+    private Integer code;
+
+    @Column(nullable = false, columnDefinition = "MEDIUMTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci")
+    private String content;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PageEntity page = (PageEntity) o;
+        return Objects.equals(relativePath, page.relativePath) && Objects.equals(site, page.site);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(relativePath, site);
+    }
+
+}
