f4291d0ec91d219307ebf587c8b9a620b7aa8a13
IrinaTik <i.tikhomirova6@gmail.com>
2024-11-14 16:45:55 +0300

    feat(jpa): добавлен слой для работы с таблицами site и page в БД
    Добавлены классы для сущностей БД site и page, а также вспомогательный enum для статуса индексации сайта.
    
    Добавлены JPA репозитории для сущностей site и page.
    
    Этап: 2

---
+++ src/main/java/searchengine/model/SiteEntity.java
@@ -0,0 +1,54 @@
+package searchengine.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+// Lombok
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+// Database
+@Entity
+@Table(name = "site")
+public class SiteEntity {
+
+    @Id
+    @Column(nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private SiteIndexingStatus status;
+
+    @Column(name = "status_time", nullable = false, columnDefinition = "DATETIME")
+    private LocalDateTime statusTime;
+
+    @Column(name = "last_error", columnDefinition = "TEXT")
+    private String lastError;
+
+    @Column(nullable = false)
+    private String url;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SiteEntity site = (SiteEntity) o;
+        return Objects.equals(url, site.url);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(url);
+    }
+
+}
